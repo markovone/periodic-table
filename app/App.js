72c336f4656503160d93c40fcde13efdebad4c5c
smarv <marko.hlavko@gmail.com>
2024-01-21 00:52:23 +0100

    basic table

--- app/App.js
+++ app/App.js
@@ -1,8 +1,24 @@
+import elems from 'App/data'
+
 export default function ()
 {
+    const eMain = elems.slice(0, 56).concat(elems.slice(70, 88)).concat(elems.slice(102, 118))
+    const eLaAc = elems.slice(56, 70).concat(elems.slice(88, 102))
+
     return (
-        <>
-            <h1>Periodic table</h1>
-        </>
+        <div className="periodic-table f-r-wrap">
+
+            { eMain.map(e => (
+                <div className="element f-c">
+                    <div className="top">
+                        { e.number }
+                    </div>
+                    <div className="symbol">
+                        { e.symbol }
+                    </div>
+                </div>
+            ))}
+
+        </div>
     )
 }
